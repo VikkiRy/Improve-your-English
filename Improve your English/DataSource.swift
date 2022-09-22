b1e32f988b47e6d17bcba2ba22d6a88be8a39fce
VikkiRy <newsvikkiry@gmail.com>
2022-09-22 21:01:50 +0300

    UI prototype

---
+++ Improve your English/DataSource.swift
@@ -0,0 +1,11 @@
+//
+//  DataSource.swift
+//  Improve your English
+//
+//  Created by Viktoria Misiulia on 21/09/2022.
+//
+
+import Foundation
+import CoreData
+
+var topics: [NSManagedObject] = []
